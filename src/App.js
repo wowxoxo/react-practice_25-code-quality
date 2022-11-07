dabc1043cf1d951fc68660fed2c8ecf2885b5fec
Petr Tsivinsky <petr.tsivinsky@iitrust.ru>
2022-11-07 13:57:04 +0300

    feat: add a11y & import plugins

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
+import React from 'react'
+
 import logo from './logo.svg'
 import './App.css'
-
-import React from 'react'
 
 function App() {
   console.log('dsd')
 
+  const arr = [
+    'John',
+    'Brad',
+    'Bob',
+    'Smith',
+    'Gerry',
+    'Gerry',
+    'John',
+    'John',
+    'John',
+    'John',
+    'John',
+    'John',
+  ]
+
+  arr.push(10)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="" />
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
